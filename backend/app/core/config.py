6b8f9457289d51940a03d8f25b60d9e1663437d4
Sumit Rathod <sumitrathod0021@gmail.com>
2026-08-30 11:47:56 +0530

    fix: make Nmap path environment configurable

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,11 +4,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     PROJECT_NAME: str
     DATABASE_URL: str
-    DEBUG: bool =False
+    DEBUG: bool = False
 
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    NMAP_PATH: str = r"C:\Program Files (x86)\Nmap\nmap.exe"
 
     model_config = SettingsConfigDict(
         env_file=".env",
